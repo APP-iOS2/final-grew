5cb86b9a634312d7da0c28db1c6449d0bfe34b0b
JCxxn <jongchan7298@naver.com>
2023-10-04 17:39:37 +0900

    Feat: Auth 초기설정

---
+++ Grew/App/MainTabView.swift
@@ -0,0 +1,51 @@
+//
+//  TabView.swift
+//  Grew
+//
+//  Created by 정유진 on 2023/10/04.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    var body: some View {
+        
+        TabView {
+            
+            Text("홈")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("홈")
+                }
+            
+            Text("내 주변")
+                .tabItem {
+                    Image(systemName: "location.fill")
+                    Text("내 주변")
+                }
+            
+            Text("모임 추가")
+                .tabItem {
+                    Image(systemName: "plus.circle")
+                        .font(.largeTitle)
+                }
+            
+            Text("채팅")
+                .tabItem {
+                    Image(systemName: "ellipsis.message")
+                    Text("채팅")
+                }
+            
+            Text("프로필")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("프로필")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
