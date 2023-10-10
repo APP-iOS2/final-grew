268037d3a0f4d30f2f4fa5a7e110ce3b860518b1
cha_nyeong <empty005@daum.net>
2023-10-10 10:49:45 +0900

    Fix: 버그 수정, 컬러 익스텐션 추가

--- Grew/Extensions/Color+Extensions.swift
+++ Grew/Extensions/Color+Extensions.swift
@@ -29,6 +29,21 @@ extension Color {
             blue: hex & 0xFF
         )
     }
+    
+    init(hexCode: String) {
+        var formattedHex = hexCode.trimmingCharacters(in: .whitespacesAndNewlines)
+        formattedHex = formattedHex.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: formattedHex).scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
 }
 
 // MARK: - Helpers
@@ -37,4 +52,40 @@ public extension Color {
     var uiColor: UIColor {
         UIColor(self)
     }
+    
+    static var Main: Color {
+            return .init(hexCode: "#25C578")
+        }
+
+        static var Sub: Color {
+            return .init(hexCode: "#FF7E00")
+        }
+
+        static var BackgroundGray: Color {
+            return .init(hexCode: "#F2F2F2")
+        }
+
+        static var LightGray1: Color {
+            return .init(hexCode: "#ECECEC")
+        }
+
+        static var LightGray2: Color {
+            return .init(hexCode: "#D4D4D4")
+        }
+
+        static var DarkGray1: Color {
+            return .init(hexCode: "#666666")
+        }
+
+        static var DarkGray2: Color {
+            return .init(hexCode: "#404040")
+        }
+
+        static var Black: Color {
+            return .init(hexCode: "#1C1C1C")
+        }
+
+        static var Error: Color {
+            return .init(hexCode: "#F05650")
+        }
 }
