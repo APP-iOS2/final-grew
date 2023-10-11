16576fca652e511b58c02831cdd9a9731586caa1
seokhoho <kreel9402@gmail.com>
2023-10-11 13:57:15 +0900

    Feat: TabView Image추가
    TabView Image추가
    탭 클릭시 색상 변환 구현

--- Grew/App/MainTabView.swift
+++ Grew/App/MainTabView.swift
@@ -9,44 +9,63 @@ import SwiftUI
 
 struct MainTabView: View {
     @State private var isNewGrewViewPresented = false
-    
+    @State private var selection: Int = 0
     var body: some View {
         NavigationStack {
-            TabView {
+            TabView(selection: $selection) {
                 
                 HomeView()
                     .tabItem {
-                        Image(systemName: "house")
+                        Image(selection == 0 ? "home_fill" : "home")
                         Text("홈")
+                            .font(.c2_B)
+                            .foregroundStyle(Color.DarkGray1)
                     }
+                    .tag(0)
 
                 Text("내 주변")
                     .tabItem {
-                        Image(systemName: "location.fill")
+                        Image(selection == 1 ? "location_fill" : "location")
                         Text("내 주변")
+                            .font(.c2_B)
+                            .foregroundStyle(Color.DarkGray1)
                     }
+                    .tag(1)
+                    
+                    
                 NewGrewView()
                     .tabItem {
-                        Image(systemName: "plus.circle")
-                            .font(.largeTitle)
+                        Image(selection == 2 ? "plus_fill" : "plus")
+                        Text("모임 생성")
+                            .font(.c2_B)
+                            .foregroundStyle(Color.DarkGray1)
                     }
+                    .tag(2)
 //                    .onAppear(perform: {
 //
 //                    })
 
                 Text("채팅")
                     .tabItem {
-                        Image(systemName: "ellipsis.message")
+                        Image(selection == 3 ? "chat_fill" : "chat")
                         Text("채팅")
+                            .font(.c2_B)
+                            .foregroundStyle(Color.DarkGray1)
                     }
+                    .tag(3)
 
                 Text("프로필")
                     .tabItem {
-                        Image(systemName: "person")
+                        Image(selection == 4 ? "profile_fill" : "profile")
                         Text("프로필")
+                            .font(.c2_B)
+                            .foregroundStyle(Color.DarkGray1)
                     }
+                    .tag(4)
             }
             .toolbar(.hidden)
+            .tint(Color.Main)
+            
         }
     }
 }
