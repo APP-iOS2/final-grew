63a66bf36c777715a5cdba3248407f359d36d3aa
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2023-10-17 03:54:31 +0900

    Feature/new grew ver3 (#39)
    * Feat: GroupMainCategoryView 생성
    
    GrewButtonModifier에 fontColor 추가
    GroupCategoryView 애니메이션 구현
    GroupSubCategoryView 애니메이션 구현
    GroupNameView 장소입력 textfield 추가
    
    * Feat: GrewButtonModifier 추가구현
    
    GrewButtonModifier에 cornerRadius 추가
    GroupCategoryView에 ButtonModifier 적용
    GroupSubCategoryView에 ButtonModifier 적용
    
    * Feat: View+Extensions 파일로 ButtonModifier 이동

---
+++ Grew/Features/Grew/GrewCategoryView/GroupCategoryView.swift
@@ -0,0 +1,73 @@
+//
+//  GroupCategoryView.swift
+//  CircleRecruitment
+//
+//  Created by 윤진영 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct GroupCategoryView: View {
+    @EnvironmentObject var viewModel: GrewViewModel
+    @Binding var selection: Selection
+    @Binding var showSubCategories: Bool
+    
+    private let gridItems: [GridItem] = [
+        //        GridItem(.adaptive(minimum: 60))
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible())
+    ]
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("그루의 카테고리를 선택해주세요")
+                    .font(.b1_R)
+                    .padding(.bottom, 10)
+                LazyVGrid(columns: gridItems) {
+                    ForEach(viewModel.categoryArray) { category in
+                        let isSelected = selection.categoryID == category.id
+                        HStack {
+                            Button {
+                                self.selection.categoryID = category.id
+                                self.selection.subCategoryID = nil
+                                viewModel.selectedCategoryId = category.id
+                                showSubCategories = true
+                            } label: {
+                                Text(category.name)
+                                    .grewButtonModifier(
+                                        width: 90,
+                                        height: 41,
+                                        buttonColor: isSelected ? Color.Sub : Color.BackgroundGray,
+                                        font: .b3_B,
+                                        fontColor: isSelected ? Color.white : Color.black,
+                                        cornerRadius: 22)
+                            }
+                            .padding(.leading, 10)
+                            .padding(.trailing, 10)
+                        }
+                    }
+                }
+            }.padding()
+        }
+    }
+}
+
+
+
+#Preview {
+    GroupCategoryView(selection: .constant(Selection(categoryID: "", subCategoryID: "")), showSubCategories: .constant(true))
+        .environmentObject(GrewViewModel())
+}
+
+/*
+ RoundedRectangle(cornerRadius: 20)
+     .foregroundStyle(isSelected ? Color.Sub : Color.BackgroundGray)
+     .cornerRadius(12)
+     .overlay(
+         Text(category.name)
+             .font(.b3_B)
+             .foregroundStyle(isSelected ? Color.white : Color.black)
+     )
+ */
--- Grew/Features/Grew/GrewStore.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GrewStore.swift
-//  Grew
-//
-//  Created by cha_nyeong on 10/9/23.
-//
-
-import Foundation
