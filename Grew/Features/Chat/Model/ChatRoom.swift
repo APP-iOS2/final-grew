e1f431179c2b372069caa6ad1a88defa5d6ef155
cha_nyeong <empty005@daum.net>
2023-10-18 01:42:45 +0900

    Feat: ChatStore, MessageStore 수정 / 모델 수정

--- Grew/Features/Chat/Model/ChatRoom.swift
+++ Grew/Features/Chat/Model/ChatRoom.swift
@@ -12,7 +12,7 @@ import Foundation
 /// 채팅방
 struct ChatRoom: Identifiable, Codable, Equatable {
     /// 채팅방 ID
-    @DocumentID var id: String?
+    var id: String
     /// 채팅방 저장 이름
     var chatRoomName: String?
     /// 참여자 UID 배열
@@ -20,12 +20,11 @@ struct ChatRoom: Identifiable, Codable, Equatable {
     /// 채팅방 생성일
     var createdDate: Date
     /// 마지막 메시지 (최신 메시지)
-    var lastMessage: String?
+    var lastMessage: String
     /// 최신 업데이트 시간
     var lastMessageDate: Date
     /// 안 읽은 메시지 개수 ( userID : 읽지 않은 메시지 개수)
     var unreadMessageCount: [String: Int]
-    
 }
 
 // 연산 프로퍼티를 나누기 위한 익스텐션
@@ -36,8 +35,6 @@ extension ChatRoom {
             return members.filter({$0 != currentUser.id})
         }
         return []
-        // guard let id = conversation.userIDs.filter({$0 != userID}).first else { return }
-        // let isRead = conversation.isRead[userID] ?? true
     }
     
     // 생성일 (년 월 일 시 분 초)
