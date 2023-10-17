9dd45dad3a6ce20e18c722b5118e09f965c14a54
Seolah Chloe Chung <seolahchloe.chung@gmail.com>
2023-10-18 00:38:38 +0900

    feat: SettingView 그루터기 카테고리 및 카테고리 기본 뷰 추가

--- Grew/Model/SettingStore.swift
+++ Grew/Model/SettingStore.swift
@@ -17,3 +17,9 @@ var appInfo: [Setting] = [
     Setting(name: "개발자 정보"),
     Setting(name: "문의하기")
 ]
+
+var grewtoegi: [Setting] = [
+    Setting(name: "그루터기 멤버 신청하기"),
+    Setting(name: "그루터기 등록하기"),
+    Setting(name: "그루터기 보기"),
+]
