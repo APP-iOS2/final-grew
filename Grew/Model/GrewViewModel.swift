cf3fba46be8a7a2ee65167cb7d25383f46e64e34
seokhoho <kreel9402@gmail.com>
2023-10-18 14:30:43 +0900

    Feat: NewestGrewList 구현
    일전 최신 그루 리스트 삭제
    최신 그루 리스트 구현
    최신 그루 셀 텍스트 수정
    최신 그루 셀 카테고리 네이밍 수정
    리스트에 셀 sort 정렬 구현

--- Grew/Model/GrewViewModel.swift
+++ Grew/Model/GrewViewModel.swift
@@ -76,8 +76,18 @@ class GrewViewModel: ObservableObject {
             }
             return ""
         }
-    /// cell에 사용할 카테고리 이름 가져오기
-    func categoryName(_ firstIndex: String, _ secondIndex: String) -> String {
+    
+    /// NewestGrewCell에 사용할 카테고리 이름 가져오기
+    func categoryName(_ categoryIndex: String) -> String {
+        
+        if let category = categoryArray.first(where: { $0.id == categoryIndex}) {
+            return category.name
+        }
+        return ""
+    }
+    
+    /// GrewCelViewl에 사용할 카테고리 이름 가져오기
+    func subCategoryName(_ firstIndex: String, _ secondIndex: String) -> String {
         
         if let category = categoryArray.first(where: { $0.id == firstIndex }) {
             if let subCategory = category.subCategories.first(where: { $0.id == secondIndex }) {
@@ -124,7 +134,7 @@ class GrewViewModel: ObservableObject {
     func popularFilter(grewList: [Grew]) -> [Grew] {
         
         let tempList = grewList.sorted(by: {$0.heartTapped > $1.heartTapped})
-        
+       
         if tempList.count < 5 {
             return tempList
         } else {
@@ -134,5 +144,12 @@ class GrewViewModel: ObservableObject {
             }
             return resultList
         }
+    }
+    
+    func newestFilter(grewList: [Grew]) -> [Grew] {
+        
+        let tempList = grewList.sorted(by: { $0.createdAt > $1.createdAt})
+        
+        return tempList
     }
 }
