a20717b2d0b93a06e7ab694384e2ad0137ad01de
Seokho Jeong <95614462+seokhoho@users.noreply.github.com>
2023-10-20 16:52:20 +0900

    Feat: 홈뷰에 네비게이션 스택 다시 활성화 (#85)
    카테고리 디테일 뷰 백버튼 구현
    홈뷰에 네비게이션 스택 다시 활성화
    홈뷰 네비게이션으로 카테고리 셀 에러 문제 해결

--- Grew/Features/Home/HomeView.swift
+++ Grew/Features/Home/HomeView.swift
@@ -11,78 +11,80 @@ struct HomeView: View {
     @EnvironmentObject var grewViewModel: GrewViewModel
     
     var body: some View {
-        ScrollView {
-            VStack {
-                // 최신 일정
-                NewestScheduleListView()
-                
-                // 카테고리 버튼
-                CategoryButtonView()
-                    .padding(.horizontal, 5)
-                    .padding(.top)
-                    .background(.white)
-                    .cornerRadius(20, corners: [.topRight, .topLeft])
-            }
-            .background(Color.Main)
-            
-            
-            VStack(alignment: .leading) {
-                
-                Divider()
-                    .frame(height: 8)
-                    .overlay(Color.BackgroundGray)
-                    .padding(.vertical)
-                Text("최신 그루")
-                    .font(.b1_B)
-                    .padding(.leading, 16)
-                    .padding(.bottom)
-                NewestGrewListView(grewList: grewViewModel.newestFilter(grewList: grewViewModel.grewList))
-                    .padding(.horizontal, 8)
-                
-                
-                Divider()
-                    .frame(height: 8)
-                    .overlay(Color.BackgroundGray)
-                    .padding(.vertical)
-                    .background(.white)
-                
-                // 인기 모임
-                Text("인기 그루")
-                    .font(.b1_B)
-                    .padding(.leading, 16)
-                GrewListView(grewList: grewViewModel.popularFilter(grewList: grewViewModel.grewList))
-                
-                
-                
-                
-            }
-        } // ScrollView
-        
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Image("logo")
-            }
-            
-            ToolbarItem(placement: .topBarTrailing) {
-                NavigationLink {
-                    Text("알림")
-                } label: {
-                    Image("alert")
-                        .font(.title2)
-                }
-            }
-            ToolbarItem(placement: .topBarTrailing) {
-                NavigationLink {
-                    GrewSearchView()
-                } label: {
-                    Image("search")
-                        .font(.title2)
-                        .padding(.trailing, 5)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    // 최신 일정
+                    NewestScheduleListView()
                     
+                    // 카테고리 버튼
+                    CategoryButtonView()
+                        .padding(.horizontal, 5)
+                        .padding(.top)
+                        .background(.white)
+                        .cornerRadius(20, corners: [.topRight, .topLeft])
+                }
+                .background(Color.Main)
+                
+                
+                VStack(alignment: .leading) {
+                    
+                    Divider()
+                        .frame(height: 8)
+                        .overlay(Color.BackgroundGray)
+                        .padding(.vertical)
+                    Text("최신 그루")
+                        .font(.b1_B)
+                        .padding(.leading, 16)
+                        .padding(.bottom)
+                    NewestGrewListView(grewList: grewViewModel.newestFilter(grewList: grewViewModel.grewList))
+                        .padding(.horizontal, 8)
+                    
+                    
+                    Divider()
+                        .frame(height: 8)
+                        .overlay(Color.BackgroundGray)
+                        .padding(.vertical)
+                        .background(.white)
+                    
+                    // 인기 모임
+                    Text("인기 그루")
+                        .font(.b1_B)
+                        .padding(.leading, 16)
+                    GrewListView(grewList: grewViewModel.popularFilter(grewList: grewViewModel.grewList))
+                    
+                    
+                    
+                    
+                }
+            } // ScrollView
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("logo")
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        Text("알림")
+                    } label: {
+                        Image("alert")
+                            .font(.title2)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        GrewSearchView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        Image("search")
+                            .font(.title2)
+                            .padding(.trailing, 5)
+                        
+                    }
                 }
             }
         }
-        
         .onAppear {
             grewViewModel.fetchGrew()
         }
