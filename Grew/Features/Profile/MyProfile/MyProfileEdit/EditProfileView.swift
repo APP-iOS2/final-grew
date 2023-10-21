845c037c7e9c64a942a9af141e85547bc59d23a4
cha_nyeong <79183499+empty005@users.noreply.github.com>
2023-10-22 03:24:57 +0900

    Feature/emergency (#108)
    * Fix: ContentListView 수정, 헤더뷰 스택 수정, 폰트 수정 (#96)
    
    * Feature/hat ver3 (#97)
    
    * Fix: ContentListView 수정, 헤더뷰 스택 수정, 폰트 수정
    
    * Fix: Profile 뷰 구조 수정
    
    * Feat: 로딩 처리 및 Profile 페이지 변경
    
    * Feat: 프로필 페이지로 전환
    
    * Feat: 프로필 페이지 헤더 추가 및 스크롤 뷰 수정
    
    * Fix: 네비게이션 스택 추가
    
    * Feat: NavigationBar 수정
    
    * Fix: Pods 오류 수정
    
    * Feat: 프로필 페이지 다른 유저 처리 진행 중
    
    * Feat: 1:1 채팅방 기능 추가
    
    * Feat: 프로필 수정 진행중
    
    ---------
    
    Co-authored-by: da-hye0 <60743139+da-hye0@users.noreply.github.com>

--- Grew/Features/Profile/MyProfile/MyProfileEdit/EditProfileView.swift
+++ Grew/Features/Profile/MyProfile/MyProfileEdit/EditProfileView.swift
@@ -23,115 +23,128 @@ struct EditProfileView: View {
     @State private var openPhoto = false
     
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .center) {
-                Button {
-                    showModal = true
-                } label: {
-                    ZStack(alignment: .bottomTrailing) {
-                        if let unwrappedImage = image {
-//                            Image(uiImage: unwrappedImage)
-                            KFImage(URL(string: user.userImageURLString ?? "chatUser"))
-                                .placeholder({
-                                    ProgressView()
-                                })
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .cornerRadius(60)
-                            
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .frame(width: 27, height: 27)
-                                .foregroundColor(Color.grewMainColor) // 이미지 색상 설정
-                                .overlay(
-                                    Circle()
-                                        .stroke(Color.white, lineWidth: 2) // 원형 보더 설정
-                                )
-                        } else {
-                            Image(uiImage: UIImage(named: "chatUser")!)
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .cornerRadius(60)
-                        }
-                        
+        VStack(alignment: .center) {
+            Button {
+                showModal = true
+            } label: {
+                ZStack(alignment: .bottomTrailing) {
+                    if let unwrappedImage = image {
+                        //                            Image(uiImage: unwrappedImage)
+                        KFImage(URL(string: user.userImageURLString ?? "defaultProfile"))
+                            .placeholder({
+                                ProgressView()
+                            })
+                            .resizable()
+                            .frame(width: 120, height: 120)
+                            .cornerRadius(60)
+                    } else {
+                        Image(uiImage: UIImage(named: "defaultProfile")!)
+                            .resizable()
+                            .frame(width: 120, height: 120)
+                            .cornerRadius(60)
                     }
-                    .padding(.vertical)
+                    
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 27, height: 27)
+                        .foregroundColor(Color.grewMainColor) // 이미지 색상 설정
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 2) // 원형 보더 설정
+                        )
+                }
+                .padding(.vertical)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("이름")
+                    .bold()
+                    .font(.title3)
+                
+                RoundedRectangle(cornerRadius: 7)
+                    .frame(height: 60)
+                    .foregroundStyle(Color.LightGray2)
+                    .overlay(alignment: .leading) {
+                        Text("\(UserStore.shared.currentUser?.nickName ?? "알 수 없음")")
+                            .padding()
+                            .foregroundColor(Color.DarkGray1)
+                    }
+                
+                
+            }
+            .padding(.vertical)
+            
+            VStack(alignment: .leading) {
+                Text("상태 메세지")
+                    .bold()
+                    .font(.title3)
+                
+                RoundedRectangle(cornerRadius: 7)
+                    .frame(height: 60)
+                    .foregroundColor(Color.BackgroundGray)
+                    .overlay(
+                        TextField("상태 메세지를 입력하세요", text: $statusMessage)
+                            .padding(.horizontal, 16)
+                        
+                    )
+                    .padding(.horizontal, 1)
+                    .padding(.vertical, 4)
+                
+                Text("생년월일 및 성별")
+                    .bold()
+                    .font(.title3)
+                    .padding(.vertical, 5)
+                
+                HStack {
+                    RoundedRectangle(cornerRadius: 7)
+                        .frame(height: 60)
+                        .foregroundColor(Color.LightGray2)
+                        .overlay(
+                            Text("\(UserStore.shared.currentUser?.dob ?? "생년월일 정보없음")")
+                                .padding(.horizontal, 16)
+                                .foregroundColor(Color.DarkGray1)
+                        )
+                        .padding(.vertical, 10)
+                    
+                    RoundedRectangle(cornerRadius: 7)
+                        .frame(width: 100, height: 60)
+                        .foregroundColor(Color.LightGray2)
+                        .overlay(
+                            Text("\(UserStore.shared.currentUser?.gender ?? "성별없음")")
+                                .padding(.horizontal, 16)
+                                .foregroundColor(Color.DarkGray1)
+                            
+                        )
+                        .padding(.horizontal, 10)
                 }
                 
-                VStack(alignment: .leading) {
-                    
-                 
-                        Text("이름")
-                        .padding(.top, 20)
-                        RoundedRectangle(cornerRadius: 8)
-                            .frame(height: 43)
-                            .foregroundStyle(Color.LightGray2)
-                            .overlay(alignment: .leading) {
-                                Text("\(UserStore.shared.currentUser?.nickName ?? "알 수 없음")")
-                                    .padding()
-                                    .foregroundColor(Color.DarkGray1)
-                            }
-                   
-                    
-                  
-                        Text("상태 메세지")
-                        .padding(.top, 20)
-                    GrewTextField(text: $statusMessage, isWrongText: false, isTextfieldDisabled: false, placeholderText: "상태 메세지를 입력하세요", isSearchBar: false)
-                        .padding(.horizontal, 1)
-                    
-                    Text("생년월일 및 성별")
-                        .padding(.top, 20)
-                    HStack {
-                        RoundedRectangle(cornerRadius: 8)
-                            .frame(height: 43)
-                            .foregroundColor(Color.LightGray2)
-                            .overlay(
-                                Text("\(UserStore.shared.currentUser?.dob ?? "생년월일 정보없음")")
-                                    .padding(.horizontal, 16)
-                                    .foregroundColor(Color.DarkGray1)
-                            )
-                            .padding(.horizontal, 1)
-                        
-                        RoundedRectangle(cornerRadius: 8)
-                            .frame(width: 100, height: 43)
-                            .foregroundColor(Color.LightGray2)
-                            .overlay(
-                                Text("\(UserStore.shared.currentUser?.gender ?? "성별없음")")
-                                    .padding(.horizontal, 16)
-                                    .foregroundColor(Color.DarkGray1)
-                            )
-                            .padding(.leading, 10)
-                    }
-                    
-                    
-                    Spacer()
+                Spacer()
+            }
+        }
+        //            .frame(maxWidth: .infinity, alignment: .leading)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button(action: {
+            self.mode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "chevron.backward")
+                .foregroundColor(Color.black)
+        }))
+        .padding()
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    // 프로필 편집
+                    saveProfileChanges()
+                    dismiss()
+                } label: {
+                    Text("저장")
+                        .foregroundStyle(.white)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(Color.grewMainColor)
+                        .cornerRadius(5)
                 }
-            }.font(.b2_R)
-            .padding(30)
-//            .frame(maxWidth: .infinity, alignment: .leading)
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading: Button(action: {
-                self.mode.wrappedValue.dismiss()
-            }, label: {
-                Image(systemName: "chevron.backward")
-                    .foregroundColor(Color.black)
-            }))
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        // 프로필 편집
-                        saveProfileChanges()
-                        dismiss()
-                    } label: {
-                        Text("저장")
-                            .foregroundColor(Color.grewMainColor)
-                            .font(.b2_B)
-                         
-                    }
-                    .buttonStyle(.plain)
-
-                }
-//                .buttonStyle(.plain)
+                .buttonStyle(.plain)
             }
         }
         .sheet(isPresented: $showModal, content: {
--- Grew/Features/Profile/ProfileView.swift
+++ Grew/Features/Profile/ProfileView.swift
@@ -2,7 +2,7 @@
 //  ProfileView.swift
 //  Grew
 //
-//  Created by Chloe Chung on 2023/09/21.
+//  Created by cha_nyeong on 2023/10/21
 //
 
 import SwiftUI
@@ -11,6 +11,7 @@ struct ProfileView: View {
     @EnvironmentObject private var grewViewModel: GrewViewModel
     @EnvironmentObject private var chatStore: ChatStore
     @EnvironmentObject private var messageStore: MessageStore
+    @Binding var selection: SelectViews
     
     @State private var isMessageAlert: Bool = false
     @State private var selectedGroup: String = "내 모임"
@@ -26,20 +27,38 @@ struct ProfileView: View {
                     
                     LazyVStack(pinnedViews: [.sectionHeaders])  {
                         Section {
-                            switch selectedFilter {
-                            case .myGroup:
-                                MyGroupView()
-                                    .background(Color.white)
-                            case .myGroupSchedule:
-                                MyGroupScheduleView()
-                                    .background(Color.white)
-                            case .savedGrew:
-                                SavedGrewView()
+                            if user == UserStore.shared.currentUser {
+                                
+                                switch selectedFilter {
+                                case .myGroup:
+                                    MyGroupView(user: user)
+                                        .background(Color.white)
+                                case .myGroupSchedule:
+                                    MyGroupScheduleView()
+                                        .background(Color.white)
+                                case .savedGrew:
+                                    SavedGrewView()
+                                        .background(Color.white)
+                                }
+                            } else {
+                                MyGroupView(user: user)
                                     .background(Color.white)
                             }
                         } header: {
-                            UserContentListView( selectedFilter: $selectedFilter)
-                                .padding(.horizontal, 10)
+                            if user == UserStore.shared.currentUser {
+                                UserContentListView( selectedFilter: $selectedFilter)
+                                    .padding(.horizontal, 10)
+                            } else {
+                                VStack{
+                                    HStack {
+                                        Text("가입한 그루")
+                                            .font(.b2_B)
+                                        Spacer()
+                                    }
+                                    .padding()
+                                }
+                                .background(Color.white)
+                            }
                         }
                     }
                 }
@@ -64,17 +83,19 @@ struct ProfileView: View {
                             }
                             .foregroundColor(.black)
                         }
-                        
                     }
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .coordinateSpace(name: "SCROLL")
                 //            .ignoresSafeArea(.container, edges: .vertical)
-                .alert("확인", isPresented: $isMessageAlert) {
+                .alert("채팅방 이동", isPresented: $isMessageAlert) {
                     Button("취소", role: .cancel) {}
                     Button("확인", role: .destructive) {
-                        startMessage()
-                        isMessageAlert = false
+                        Task {
+                            await startMessage()
+                            isMessageAlert = false
+                            selection = .chat
+                        }
                     }
                 } message: {
                     Text("1:1 채팅방으로 이동합니다.")
@@ -91,13 +112,7 @@ struct ProfileView: View {
             .background(Color.Main)
             .navigationBarBackground(.Main)
         }
-        
     }
-    // 1:1 메시지 생성
-    private func startMessage() {
-        
-    }
-    
     @ViewBuilder
     private func headerView() -> some View {
         var backgroundHeight: CGFloat {
@@ -135,12 +150,14 @@ struct ProfileView: View {
                                 user: user ?? User.dummyUser
                             )
                         } label: {
-                            Text("프로필 수정")
-                                .font(.c1_B)
-                                .background(RoundedRectangle(cornerRadius: 7)
-                                    .foregroundColor(.LightGray2)
-                                    .frame(width: 90, height: 28)
-                                )
+                            if user == UserStore.shared.currentUser {
+                                Text("프로필 수정")
+                                    .font(.c1_B)
+                                    .background(RoundedRectangle(cornerRadius: 7)
+                                        .foregroundColor(.LightGray2)
+                                        .frame(width: 90, height: 28)
+                                    )
+                            }
                         }
                         .padding(10)
                         .foregroundColor(.white)
@@ -159,6 +176,33 @@ struct ProfileView: View {
 
 #Preview {
     NavigationStack{
-        ProfileView(user: User.dummyUser)
+        ProfileView(selection: .constant(.profile), user: User.dummyUser)
+    }
+}
+
+
+extension ProfileView {
+    
+    // 1:1 메시지 생성
+    private func startMessage() async {
+        // 1. 채팅방을 생성하고 인원을 넣는다.
+        let chatRoom = await makeChatRoomForNewRoom()
+        await chatStore.addChatRoom(chatRoom)
+        // 2. 시스템 메시지를 추가한다.
+        var newMessage = ChatMessage(text: "\(user?.nickName) 님과 \(UserStore.shared.currentUser?.nickName)의 대화가 시작되었습니다.", uid: "system", userName: "시스템 메시지", isSystem: true)
+        
+        messageStore.addMessage(newMessage, chatRoomID: chatRoom.id)
+        
+    }
+    
+    private func makeChatRoomForNewRoom() async -> ChatRoom {
+        var newChatRoom: ChatRoom = ChatRoom(
+            id: UUID().uuidString,
+            members: [user!.id!, UserStore.shared.currentUser!.id!],
+            createdDate: Date(),
+            lastMessage: "\(user?.nickName) 님과 \(UserStore.shared.currentUser?.nickName)의 대화가 시작되었습니다.",
+            lastMessageDate: Date(),
+            unreadMessageCount: [:])
+        return newChatRoom
     }
 }
