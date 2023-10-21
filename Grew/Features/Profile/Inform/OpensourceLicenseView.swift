c079f28b2a89859949af56882fe4d815a6dfb706
JCxxn <jongchan7298@naver.com>
2023-10-21 14:16:35 +0900

    Feat: 라이센스, 정보, 문의하기 웹뷰 추가

--- Grew/Features/Profile/Inform/OpensourceLicenseView.swift
+++ Grew/Features/Profile/Inform/OpensourceLicenseView.swift
@@ -5,11 +5,38 @@
 //  Created by Chloe Chung on 2023/10/18.
 //
 
+import AcknowList
 import SwiftUI
 
 struct OpensourceLicenseView: View {
+     
+     @Environment(\.dismiss) private var dismiss
+     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+
     var body: some View {
-        Text("OpensourceLicenseView")
+        VStack {
+            LicenseView()
+        }
+        .navigationTitle("오픈소스 라이센스")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .navigationBarItems(leading: Button(action: {
+            self.mode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "chevron.backward")
+                .foregroundColor(.black)
+        }))
+    }
+}
+
+struct LicenseView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let acknowledgementsViewController = AcknowListViewController()
+        return acknowledgementsViewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        // No update needed
     }
 }
 
