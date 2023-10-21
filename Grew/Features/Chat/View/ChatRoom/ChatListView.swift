f72a4177fa28e648c054437e759ddef7ea866c8c
cha_nyeong <empty005@daum.net>
2023-10-21 21:24:38 +0900

    Feat: 로딩 처리 및 Profile 페이지 변경

--- Grew/Features/Chat/View/ChatRoom/ChatListView.swift
+++ Grew/Features/Chat/View/ChatRoom/ChatListView.swift
@@ -12,76 +12,69 @@ struct ChatListView: View {
     @EnvironmentObject private var chatStore: ChatStore
     
     var body: some View {
-        ScrollView {
+        VStack{
             switch filter {
             case .group:
                 if chatStore.groupChatRooms.isEmpty {
-                    isEmptyGroup()
+                    isEmptyGroup
                 } else {
-                    ForEach(chatStore.groupChatRooms){ chatRoom in
-                        NavigationLink {
-                            ChatDetailView(
-                                chatRoom: chatRoom,
-                                targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? []
-                            )
-                        } label: {
-                            ChatRoomCell(
-                                chatRoom: chatRoom,
-                                chatGrewInfo: chatStore.targetGrewInfoDict[chatRoom.grewId ?? ""],
-                                targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? []
-                            )
+                    ScrollView {
+                        ForEach(chatStore.groupChatRooms){ chatRoom in
+                            NavigationLink {
+                                ChatDetailView(
+                                    chatRoom: chatRoom,
+                                    targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? []
+                                )
+                            } label: {
+                                ChatRoomCell(
+                                    chatRoom: chatRoom,
+                                    chatGrewInfo: chatStore.targetGrewInfoDict[chatRoom.grewId ?? ""],
+                                    targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? []
+                                )
+                            }
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 15)
+                            .listSectionSeparator(.hidden)
+                            .foregroundColor(.black)
                         }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 15)
-                        .listSectionSeparator(.hidden)
-                        .foregroundColor(.black)
                     }
                 }
             case .personal:
                 if chatStore.personalChatRooms.isEmpty {
-                    isEmptyGroup()
+                    isEmptyGroup
                 } else {
-                    ForEach(chatStore.personalChatRooms){ chatRoom in
-                        NavigationLink {
-                            ChatDetailView(chatRoom: chatRoom, targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? [])
-                        } label: {
-                            ChatRoomCell(
-                                chatRoom: chatRoom,
-                                targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? []
-                            )
+                    ScrollView {
+                        ForEach(chatStore.personalChatRooms){ chatRoom in
+                            NavigationLink {
+                                ChatDetailView(chatRoom: chatRoom, targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? [])
+                            } label: {
+                                ChatRoomCell(
+                                    chatRoom: chatRoom,
+                                    targetUserInfos: chatStore.targetUserInfoDict[chatRoom.id] ?? []
+                                )
+                            }
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 15)
+                            .listSectionSeparator(.hidden)
+                            .foregroundColor(.black)
                         }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 15)
-                        .listSectionSeparator(.hidden)
-                        .foregroundColor(.black)
                     }
                 }
             }
         }
-        .task {
-            if !chatStore.isDoneFetch {
-                chatStore.addListener()
-                await chatStore.fetchChatRooms()
-            }
-        }
-        .onDisappear {
-            chatStore.removeListener()
-            chatStore.isDoneFetch = false
-            
-        }
     }
-    @ViewBuilder
-    private func isEmptyGroup() -> some View {
+    
+    private var isEmptyGroup: some View {
         VStack {
             Spacer()
             switch filter {
             case .personal:
-                Text("참여 중인 그루 채팅방이 없습니다.")
+                Text("참여 중인 개인 채팅방이 없습니다.")
                     .font(.h2_B)
                 Image(systemName: "bubble.left.and.text.bubble.right.fill")
                     .font(.title2)
             case .group:
-                Text("참여 중인 개인 채팅방이 없습니다.")
+                Text("참여 중인 그루 채팅방이 없습니다.")
                     .font(.h2_B)
                 Image(systemName: "bubble.left.and.text.bubble.right.fill")
                     .font(.title2)
--- Grew/Features/Profile/MyProfile/ProfileHeaderView.swift
+++ Grew/Features/Profile/MyProfile/ProfileHeaderView.swift
@@ -11,8 +11,8 @@ import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ProfileHeaderView: View {
-    @State var name: String
-    @State var statusMessage: String
+    
+    let user: User
     
     private var backgroundHeight: CGFloat {
         //        let count = CGFloat(ProfileThreadFilter.allCases.count)
@@ -21,54 +21,55 @@ struct ProfileHeaderView: View {
     }
     
     var body: some View {
-            ZStack(alignment: .bottom) {
-                RoundSpecificCorners()
-
-                VStack(alignment: .leading) {
-                    Spacer()
-          
-                    CircleImage()
-                        .padding(50)
-                   
-                    HStack(alignment: .bottom){
-
-                        VStack(alignment: .leading){
-                            Text(UserStore.shared.currentUser?.nickName ?? "이름없음")
-                                .font(.b1_R)
-                            Text(UserStore.shared.currentUser?.introduce ?? "안녕하세요 \(UserStore.shared.currentUser?.nickName ?? "이름없음")입니다.")
-                                .font(.b3_L)
-                                .padding(.vertical, 5)
-                        }
-                        Spacer()
-
-                        NavigationLink {
-                            EditProfileView(name: UserStore.shared.currentUser?.nickName ?? "",
-                                            statusMessage: UserStore.shared.currentUser?.introduce ?? "")
-                        } label: {
-                            Text("프로필 수정")
-                                .font(.c1_B)
-                                .background(RoundedRectangle(cornerRadius: 7)
-                                    .foregroundColor(.LightGray2)
-                                    .frame(width: 90, height: 28)
-                                )
-                        }
-                        .padding(10)
-                        .foregroundColor(.white)
-                    }.padding(10)
-                        .padding(.horizontal, 10)
+        ZStack(alignment: .bottom) {
+            RoundSpecificCorners()
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                
+                CircleImage()
+                    .padding(50)
+                
+                HStack(alignment: .bottom){
                     
-                    Divider()
-                }
-                   
-            }.background(Color.grewMainColor)
+                    VStack(alignment: .leading){
+                        Text(user.nickName)
+                            .font(.b1_R)
+                        Text(user.introduce ?? "안녕하세요 \(user.nickName)입니다.")
+                            .font(.b3_L)
+                            .padding(.vertical, 5)
+                    }
+                    Spacer()
+                    
+                    NavigationLink {
+                        EditProfileView(
+                            user: user
+                        )
+                    } label: {
+                        Text("프로필 수정")
+                            .font(.c1_B)
+                            .background(RoundedRectangle(cornerRadius: 7)
+                                .foregroundColor(.LightGray2)
+                                .frame(width: 90, height: 28)
+                            )
+                    }
+                    .padding(10)
+                    .foregroundColor(.white)
+                }.padding(10)
+                    .padding(.horizontal, 10)
+                
+                Divider()
+            }
+            
+        }.background(Color.grewMainColor)
             .frame(height: UIScreen.main.bounds.height/7*2)
-
+        
     }
     
 }
 
 #Preview {
     NavigationStack {
-        ProfileHeaderView(name: "헬롱", statusMessage: "하위")
+        ProfileHeaderView(user: UserStore.shared.currentUser!)
     }
 }
