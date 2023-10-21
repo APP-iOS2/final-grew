daa7358ae7a432b2f39bd799580abb6be1018dde
Kyoungmi <1mmgm.dev@gmail.com>
2023-10-22 00:29:17 +0900

    [feature] modify the user initializer

--- Grew/Model/User.swift
+++ Grew/Model/User.swift
@@ -9,7 +9,7 @@ import Firebase
 import FirebaseFirestoreSwift
 import Foundation
 
-struct User: Identifiable, Codable {
+struct User: Identifiable, Codable, Hashable {
     /// 유저의 Auth 고유 아이디.
     @DocumentID var id: String? /*= UUID().uuidString*/
     /// 유저 이름
